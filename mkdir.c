06cca70768f6f0f6da1e16a437d911092119eaa8
Dekoder-py <kyle@codingcorner.dev>
2026-03-17 11:52:23 +1300

    mkdir

---
+++ mkdir.c
@@ -0,0 +1,20 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "mkdir: no arguments provided.\n");
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (mkdir(argv[i], 0777) != 0) {
+      fprintf(stderr, "mkdir: unable to create directory '%s': %s\n", argv[i],
+              strerror(errno));
+    }
+  }
+}
